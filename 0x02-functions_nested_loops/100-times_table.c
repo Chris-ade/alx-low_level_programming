23c26ef8a763df3be6e899ec816c3d38f82bc53e
Chris-ade <joeade12054@gmail.com>
2022-04-12 11:03:29 -0700

    C - Functions, nested loops - Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table
  * @n : times table to use
  * Return:void
  */
 
-int print_times_table(int n)
+void print_times_table(int n)
 {
 
 int a = 0, rep, b;
