bca3f02100e38d2882b80d7d120dee60e3900ca1
Chris-ade <joeade12054@gmail.com>
2022-05-26 11:00:48 -0700

    C - More singly linked lists - Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
-* pop_listint - deletes the head node of a linked list,returns the head node’s data.
+* pop_listint - deletes the head node of a linked list
 * @head: pointer double.
 * Return: data.
 */
